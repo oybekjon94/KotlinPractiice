33e0fb875f408090e941a0bb161e73a6f851f9d2
oybekjon94 <x.oybek94@gmail.com>
2022-11-29 12:37:18 +0900

    [feat::] Add 3 array

--- src/main/kotlin/array/3.kt
+++ src/main/kotlin/array/3.kt
@@ -1,2 +1,19 @@
 package array
 
+fun main(){
+    val array = intArrayOf(2,2,23,4,5,5,2,3,4)
+    val result =  IntArray(array.size*2)
+
+    result[0] = 0
+    result[1] = array.first()
+    var count = 2
+    for (index in 1 until array.size){
+        if (array[index] == array[index - 1]){
+            result[count++] = array[index]
+        }else{
+            result[count++] = 0
+            result[count++] = array[index]
+        }
+    }
+    println(result.joinToString())
+}
